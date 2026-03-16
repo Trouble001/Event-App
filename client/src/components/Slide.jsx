2807713722f125fb0715655c305cb288c6c37a03
staytrouble <gschaturvedi001@gmail.com>
2026-03-16 07:46:59 +0530

    Styling slide

--- client/src/components/Slide.jsx
+++ client/src/components/Slide.jsx
@@ -1,10 +1,20 @@
 const Slide = ({ slide, active }) => {
 
   return (
-    <div className={`absolute w-full h-full flex items-center justify-center transition-opacity duration-700 ${active ? "opacity-100" : "opacity-0"}`}>
-      <div className="relative text-center text-white bg-white/20 ">
-        <h2 className="text-4xl font-bold mb-4">{slide.title}</h2>
-        <p className="text-xl">{slide.text}</p>
+    <div
+      className={`absolute top-0 w-full pb-20 md:pb-0 md:pl-22 pl-2 pr-2 bg-black/80 h-full flex items-center justify-center transition-all duration-700 ${active ? "opacity-100" : "opacity-0"}`}
+      style={{ 
+        backgroundImage: `url(${slide.image})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+      }}
+      >
+      <div className="relative text-center backdrop-blur-sm bg-black/70 border border-white/30 shadow-2xl rounded-2xl p-8">
+        <h2 className="text-2xl md:text-3xl text-white font-bold mb-4">{slide.title}</h2>
+        <h2 className="text-md md:text-xl text-white/90 mb-2">{slide.subtitle}</h2>
+        <p className="text-sm md:text-md text-white/70">{slide.text}</p>
       </div>
     </div>
   );
--- client/src/components/SlideShow.jsx
+++ client/src/components/SlideShow.jsx
@@ -1,22 +1,51 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Slide from "./Slide";
-import AppLayout from "../layouts/AppLayout";
+import { PauseIcon, PlayIcon } from '@heroicons/react/24/solid';
 
 const SlideShow = ({ slides }) => {
   const [current, setCurrent] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [paused, setPaused] = useState(false);
+
+  const elapsedRef = useRef(0);
+  const startRef = useRef(null);
+
   useEffect(() => {
-    if (!slides.length) return;
+    if (!slides.length || paused) return;
+
     const duration = slides[current].duration || 6000;
-    const timer = setTimeout(() => {
-      if (current < slides.length - 1) {
-        setCurrent(current + 1);
+
+    startRef.current = Date.now() - elapsedRef.current;
+
+    const interval = setInterval(() => {
+      const elapsed = Date.now() - startRef.current;
+      elapsedRef.current = elapsed;
+      const percent = (elapsed / duration) * 100;
+      setProgress(percent);
+      if (elapsed >= duration) {
+        elapsedRef.current = 0;
+        setProgress(0);
+
+        setCurrent((prev) =>
+          prev === slides.length - 1 ? 0 : prev + 1
+        );
       }
-    }, duration);
-    return () => clearTimeout(timer);
-  }, [current, slides]);
+    }, 50);
+    return () => clearInterval(interval);
+  }, [current, slides, paused]);
+
+
+  const togglePause = () => {
+    if (!paused) {
+      // store elapsed when pausing
+      elapsedRef.current = Date.now() - startRef.current;
+    }
+    setPaused(!paused);
+  };
   return (
-    <AppLayout>
-        <div className="w-full bg-green-400 relative">
+    <div className="relative w-full h-screen overflow-hidden"
+      onClick={togglePause}>
+
       {slides.map((slide, index) => (
         <Slide
           key={slide.id}
@@ -24,8 +53,26 @@ const SlideShow = ({ slides }) => {
           active={index === current}
         />
       ))}
+
+      {/* Progress bar */}
+      <div className="absolute bottom-11 md:bottom-0 left-0 w-full h-2 bg-black/40 backdrop-blur-md">
+        <div
+          className="h-full bg-white/60 backdrop-blur-md rounded-2xl"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+
+      {/* Pause / Resume */}
+      <div className="absolute w-full bottom-14 flex items-center justify-center md:pl-20 z-10">
+        <button
+          onClick={togglePause}
+          className="backdrop-blur-md shadow-2xl text-white p-2 text-center rounded-full font-bold flex items-center justify-center"
+        >
+          {paused ? (<PlayIcon className="size-8" />) : (<PauseIcon className="size-8" />)}
+        </button>
+      </div>
+
     </div>
-    </AppLayout>
   );
 };
 
